0c8ae738064dbf6e0d38aff216b8b426b4cf6332
Konrad Gądek <kgadek@gmail.com>
2024-11-15 00:15:51 +0100

    Remove comments; gofmt -s -w

--- cmd/combine.go
+++ cmd/combine.go
@@ -9,16 +9,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// combineCmd represents the combine command
 var combineCmd = &cobra.Command{
-	Use:   "combine",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "combine",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("combine called")
 	},
@@ -26,14 +18,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(combineCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// combineCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// combineCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/split.go
+++ cmd/split.go
@@ -11,14 +11,7 @@ import (
 
 // splitCmd represents the split command
 var splitCmd = &cobra.Command{
-	Use:   "split",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "split",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("split called")
 	},
@@ -26,14 +19,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(splitCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// splitCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// splitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
